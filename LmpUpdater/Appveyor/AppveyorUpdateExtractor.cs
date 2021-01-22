7d2a567b3395f047fffbdd721a55c24458fac97c
storm37000 <6331913+storm37000@users.noreply.github.com>
2021-01-22 18:07:06 -0500

    cleanup

--- LmpUpdater/Appveyor/AppveyorUpdateExtractor.cs
+++ LmpUpdater/Appveyor/AppveyorUpdateExtractor.cs
@@ -1,5 +1,4 @@
-﻿using Ionic.Zip;
-using System;
+﻿using System;
 using System.IO;
 
 namespace LmpUpdater.Appveyor
@@ -11,29 +10,26 @@ namespace LmpUpdater.Appveyor
             if (string.IsNullOrEmpty(zipFilePath) || string.IsNullOrEmpty(destinationFolder) || !Directory.Exists(destinationFolder) || !File.Exists(zipFilePath))
                 return;
 
-            using (var zipFile = ZipFile.Read(zipFilePath))
+            var tempFolder = Path.Combine(destinationFolder, "TempUnzipFolder");
+            Directory.CreateDirectory(tempFolder);
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipFilePath, tempFolder, true);
+
+            switch (product)
             {
-                var tempFolder = Path.Combine(destinationFolder, "TempUnzipFolder");
-                Directory.CreateDirectory(tempFolder);
-                zipFile.ExtractAll(tempFolder, ExtractExistingFileAction.OverwriteSilently);
-
-                switch (product)
-                {
-                    case AppveyorProduct.Client:
-                        ExtractClient(tempFolder);
-                        break;
-                    case AppveyorProduct.Server:
-                        ExtractServer(tempFolder);
-                        break;
-                    case AppveyorProduct.MasterServer:
-                        ExtractMasterServer(tempFolder);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(product), product, null);
-                }
-
-                Directory.Delete(tempFolder, true);
+                case AppveyorProduct.Client:
+                    ExtractClient(tempFolder);
+                    break;
+                case AppveyorProduct.Server:
+                    ExtractServer(tempFolder);
+                    break;
+                case AppveyorProduct.MasterServer:
+                    ExtractMasterServer(tempFolder);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(product), product, null);
             }
+
+            Directory.Delete(tempFolder, true);
 
             File.Delete(zipFilePath);
         }
--- LmpUpdater/Github/GithubUpdateExtractor.cs
+++ LmpUpdater/Github/GithubUpdateExtractor.cs
@@ -1,5 +1,4 @@
-﻿using Ionic.Zip;
-using System;
+﻿using System;
 using System.IO;
 
 namespace LmpUpdater.Github
@@ -11,26 +10,23 @@ namespace LmpUpdater.Github
             if (string.IsNullOrEmpty(zipFilePath) || string.IsNullOrEmpty(destinationFolder) || !Directory.Exists(destinationFolder) || !File.Exists(zipFilePath))
                 return;
 
-            using (var zipFile = ZipFile.Read(zipFilePath))
+            var tempFolder = Path.Combine(destinationFolder, "TempUnzipFolder");
+            Directory.CreateDirectory(tempFolder);
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipFilePath, tempFolder, true);
+
+            switch (product)
             {
-                var tempFolder = Path.Combine(destinationFolder, "TempUnzipFolder");
-                Directory.CreateDirectory(tempFolder);
-                zipFile.ExtractAll(tempFolder, ExtractExistingFileAction.OverwriteSilently);
-
-                switch (product)
-                {
-                    case GithubProduct.Client:
-                        ExtractClient(tempFolder);
-                        break;
-                    case GithubProduct.Server:
-                        ExtractServer(tempFolder);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(product), product, null);
-                }
-
-                Directory.Delete(tempFolder, true);
+                case GithubProduct.Client:
+                    ExtractClient(tempFolder);
+                    break;
+                case GithubProduct.Server:
+                    ExtractServer(tempFolder);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(product), product, null);
             }
+
+            Directory.Delete(tempFolder, true);
 
             File.Delete(zipFilePath);
         }
